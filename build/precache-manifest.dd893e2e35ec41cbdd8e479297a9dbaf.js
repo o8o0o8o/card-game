7a3a0e05c9b846e812485c4ba4f95cb5817ddccb
Siarhei Labuts <sergeylabut@gmail.com>
2020-07-24 22:11:22 +0300

    build commit

---
+++ build/precache-manifest.dd893e2e35ec41cbdd8e479297a9dbaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451f89d5cae588c67003f362d070c7ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f5b6e3f02e9a312ab7b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8160d28907fc96e60efa",
+    "url": "/static/js/2.4f8283d3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4f8283d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5b6e3f02e9a312ab7b",
+    "url": "/static/js/main.fafa3237.chunk.js"
+  },
+  {
+    "revision": "2f0eea6fc9ca379128db",
+    "url": "/static/js/runtime-main.b2e03d6a.js"
+  }
+]);
