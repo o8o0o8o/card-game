12a7e21317bcb5c5614d80b8c8639100c3d8b079
Siarhei Labuts <sergeylabut@gmail.com>
2020-07-24 23:20:20 +0300

    fix

--- src/App/components/BlackJack/BlackJack.js
+++ src/App/components/BlackJack/BlackJack.js
@@ -139,32 +139,32 @@ export const BlackJack = () => {
       <Errors />
       <div className={classes.chipHolder}>
         <ChipHolder />
-      <div className={classes.moneyComponents}>
-        <div className={classes.chipHolder}>
-          <ChipHolder />
+        <div className={classes.moneyComponents}>
+          <div className={classes.chipHolder}>
+            <ChipHolder />
+          </div>
+          <div>{`Player money ${playerMoney}`}</div>
+          <div>{`Player bet ${playerBet}`}</div>
         </div>
-        <div>{`Player money ${playerMoney}`}</div>
-        <div>{`Player bet ${playerBet}`}</div>
-      </div>
-      <div>{`Player score ${playerScore}`}</div>
-      <div>{`Dealer score ${dealerScore}`}</div>
-      <div>{lastGameResult}</div>
-      <div className={classes.playerHand}>
-        <PlayerHand />
-      </div>
-      <div className={classes.dealerHand}>
-        <DealerHand />
-      </div>
-
-      <div className={classes.buttonsContainer}>
-        <div className={classes.buttonsSplit}>
-          <Button text="Start" callback={startGame} />
-          <Button text="Hit" callback={addCardToPlayerHand} />
-          <Button text="Stand" callback={addCardToDealerHand} />
+        <div>{`Player score ${playerScore}`}</div>
+        <div>{`Dealer score ${dealerScore}`}</div>
+        <div>{lastGameResult}</div>
+        <div className={classes.playerHand}>
+          <PlayerHand />
         </div>
-      </div>
-      <div className={classes.footer}>
-        <Footer />
+        <div className={classes.dealerHand}>
+          <DealerHand />
+        </div>
+        <div className={classes.buttonsContainer}>
+          <div className={classes.buttonsSplit}>
+            <Button text="Start" callback={startGame} />
+            <Button text="Hit" callback={addCardToPlayerHand} />
+            <Button text="Stand" callback={addCardToDealerHand} />
+          </div>
+        </div>
+        <div className={classes.footer}>
+          <Footer />
+        </div>
       </div>
     </div>
   );
